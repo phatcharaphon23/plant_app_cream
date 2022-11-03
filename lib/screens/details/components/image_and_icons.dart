f3d4224c64085a74669948e93d8c1e814101c8d8
phatcharaphon23 <phatcharaphon.h@ku.th>
2022-11-03 17:25:11 +0700

    addnew

--- lib/screens/details/components/image_and_icons.dart
+++ lib/screens/details/components/image_and_icons.dart
@@ -1,9 +1,8 @@
-import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
 import '../../../constants.dart';
-import '../../../data/canabisdata.dart';
+
 import 'image_slider.dart';
 
 class ImageAndIcons extends StatefulWidget {
